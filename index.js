60b72beb4bbe9679c9021471e59ae917a606ae0d
mrr <broitman.rom@gmail.com>
2025-08-08 08:31:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
