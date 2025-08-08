c314748a9becee8cf5c5ed150680fea165339f85
mrr <broitman.rom@gmail.com>
2025-08-08 08:53:42 +0300

    update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000
 })
 .then(() => console.log('✅ MongoDB connected'))
