7c97f45785e1418e2b4464d5d20a90912ccc200b
mrr <broitman.rom@gmail.com>
2025-08-08 09:15:33 +0300

    update index.js and statusRoutes.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
