52bc40bd30225e5ae87c1b4c32f1d3da20554da1
mrr <broitman.rom@gmail.com>
2025-08-08 08:24:54 +0300

    update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
