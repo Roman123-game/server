08c8bf411b65c018362d014f9944b495a10821b5
mrR <broitman.rom@gmail.com>
2026-07-09 21:35:15 +0300

    update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// router.post('/', userController.saveGoogleUser);
+router.post('/', userController.saveGoogleUser);
 // router.get('/', userController.getUsers);
-router.post('/', (req, res, next) => {
-	console.log("USER ROUTE HIT");
-	next();
-}, userController.saveGoogleUser);
+
 
 module.exports = router;
